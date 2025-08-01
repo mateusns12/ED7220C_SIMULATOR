27bf880b70b55d148c09232644d58d718ba95452
mateusns12 <mateusgms12@gmail.com>
2025-08-01 17:42:58 -0300

    Implemented Remaining Instructions to Emitter
    Added Runtime namespace to manage runtime events and errors.
    Added power operation to Evaluator.
    
    The COM port is now the second command line argument, and the
    port is set before the Interpreter is created.
    
    Added Register function to Emitter using the Switch object,
    so functions are declared once.
    
    Implemented remaining functions to handle the Robot Instructions

--- lua_standalone/interpreter.lua
+++ lua_standalone/interpreter.lua
@@ -5,19 +5,14 @@ SerialPort = require "serialport"
 
 Evaluator = {}
 Interpreter = {}
-Symbols = {
-    points = {},
-    globals = {},
-    subs = {},
-    labels = {},
-    fors = {}
-}
-
+Emitter = {}
+Runtime = {}
 local f = string.format
 
-function Interpreter:new(op)
-    Interpreter.__index = Interpreter
-    return setmetatable({ops = op, connected = false, port = nil},Interpreter)
+function Runtime.assert(condition,cmd,msg)
+    if not condition then
+        print(f("\x1b[91m[RUNTIME ERROR]\x1b[0m \x1b[92;2mAt Instruction: \x1b[0m\x1b[93m%s\x1b[0m %s",cmd,msg))
+    end
 end
 
 function Evaluator:new()
@@ -46,6 +41,8 @@ function Evaluator:eval(expr)
             return eval(expr[2][1]) > eval(expr[2][2])
         elseif expr[1]=='LT' then
             return eval(expr[2][1]) < eval(expr[2][2])
+        elseif expr[1]=='POW' then
+            return eval(expr[2][1]) ^ eval(expr[2][2])
         elseif expr[1]=='NEG' then
             return -eval(expr[2])
         else assert(false,f("%s,Something wrong i can feel it",expr[1]))
@@ -54,13 +51,14 @@ function Evaluator:eval(expr)
     return eval(expr)
 end
 
+function Interpreter:new(op,em)
+    Interpreter.__index = Interpreter
+    return setmetatable({ops = op, connected = false, emitter = em},Interpreter)
+end
+
 function Interpreter:run()
     local eval = Evaluator:new()
-    local serial = SerialPort:new("COM1")
-    local emit = Emitter:new()
-    emit:setport(serial)
     local idx = 1
-    local count = 1
     local advance = function() idx = idx + 1 end
     while idx <= #self.ops do
         local cmd = self.ops[idx]
@@ -74,7 +72,8 @@ function Interpreter:run()
             if result == false then advance() end   
         elseif cmd[1] == 'GOTO' then
             idx = Symbols.labels[cmd[2]]
-        elseif cmd[1] == 'SETI' then
+        elseif cmd[1] == 'SETI'
+            or cmd[1] == 'ASSIGN'then
             local var = Symbols.globals[cmd[2]]
             if not var then push(Symbols.globals[cmd[2]],{'NUM',eval:eval(cmd[3])})
             else var[#var] = {'NUM',eval:eval(cmd[3])}
@@ -83,7 +82,9 @@ function Interpreter:run()
             run_cmd = 0
         elseif cmd[1] == 'LOCAL' then
             local sub = Symbols.subs
+            Runtime.assert(sub[#sub],'LOCAL',"Local variable outside of a subroutine")
             push(sub[#sub],cmd[2])
+            push(Symbols.globals[cmd[2]],{'NUM',0})
         elseif cmd[1] == 'GOSUB' then
             push(Symbols.subs,{cmd[2],idx})
             idx = Symbols.labels[cmd[2]]
@@ -113,30 +114,19 @@ function Interpreter:run()
         ------------------------------------------------
         elseif cmd[1] == 'CLS' then
         ------------------------------------------------
-        elseif cmd[1] == 'PAUSE'
-            or cmd[1] == "MOVEP"
-            or cmd[1] == "OPEN"
-            or cmd[1] == "CLOSE"
-
-            or cmd[1] == "HOME"
-            or cmd[1] == "HARDHOME"
-            or cmd[1] == "VEL"
-            or cmd[1] == "SEND"
-            or cmd[1] == "OUTSIG" then
-            
-            emit:exec(cmd)
+        elseif cmd[1] == 'IFSIG' then
+            self.emitter:exec(cmd) 
+            if cmd[3] == 0 then advance() end   
         else
-            assert(false,cmd[1] ..' Not implemented')
+            self.emitter:exec(cmd)     
         end
         advance()
     end
 end
 
-Emitter = {}
-
 function Emitter:new()
     Emitter.__index = Emitter
-    return setmetatable({port = nil},Emitter)
+    return setmetatable({port = nil,eval = Evaluator:new(),motors = {'B','C','D','E','F'}},Emitter)
 end
 
 function Emitter:setport(port)
@@ -160,14 +150,22 @@ function Emitter:wait(delay)
     return self.port:wait(delay)
 end
 
-function Emitter:exec(inst)
-    local motors = {'B','C','D','E','F'}
-    local eval = Evaluator:new()
+function Emitter:register()
     local function set_position(point)
-        for i=1,#motors do
-            self:write(f("pd,%s,%d\r",motors[i],eval:eval(point[i])))
-            self:wait(10)
+        for i=1,#self.motors do
+            self:write(f("pd,%s,%d\r",self.motors[i],self.eval:eval(point[i])))
+            self:wait(50)
         end
+    end
+    local function get_position()
+        local pos = {}
+        for i=1,#self.motors do
+            self:write(f("pa,%s\r",self.motors[i]))
+            self:wait(100)
+            local res,msg = self:read()
+            push(pos,msg)
+        end
+        return pos
     end
     local function motor_busy()
         self:write('ss\r')
@@ -182,20 +180,26 @@ function Emitter:exec(inst)
         self:wait(100)
         ret,msg = self:read()
     end
-    local function movep(args)
+    local function move(args)
         local point = Symbols.points[args[2]]
-
-        set_position(point)
-        
+        for i=1,#self.motors do
+            self:write(f("pr,%s,%d\r",self.motors[i],self.eval:eval(point[i])))
+            self:wait(50)
+        end
         self:write("mc\r")
-        self:wait(200)
+        self:wait(100)
         while (motor_busy()==1) do
         end
-        for i=1,#motors do
-            print(f("pa,%s\r",motors[i]))
-            -- Check Return == Set
+        local final_pos = get_position()
+    end
+    local function movep(args)
+        local point = Symbols.points[args[2]]
+        set_position(point)
+        self:write("mc\r")
+        self:wait(100)
+        while (motor_busy()==1) do
         end
-        -- If ss == 0 Proceed
+        local final_pos = get_position()
     end
     local function open()
         self:write("go\r") 
@@ -218,22 +222,21 @@ function Emitter:exec(inst)
     local function home(args)
         set_position({{'NUM',0},{'NUM',0},{'NUM',0},{'NUM',0},{'NUM',0}})
         self:write("mc\r")   -- Start
-        self:wait(200)
+        self:wait(100)
         while(motor_busy()==1)do
         end
-        for i=1,#motors do
-            print(f("pa,%s\r",motors[i]))
-            -- Check Return == Set
-        end
-        -- If ss == 0 Proceed
+        local pos = get_position()
     end
     local function pause(args)
         local time = eval:eval(args[2])
         self:wait(time*1000)
     end
     local function hardhome(args)
-        print("hh\r")
+        self:write("hh\r")
+        while(motor_busy()==1)do
+        end
         -- Loop check_system_status until Finish
+        local pos = get_position()
     end
     local function vel(args)
         self:write(f("vs,%s,%d",args[2][1],eval:eval(args[2][2])))
@@ -242,23 +245,88 @@ function Emitter:exec(inst)
     local function outsig(args)
     end
     local function send(args)
-        print("tc\r")
-        print("ts,1,1\r")
-        print(f("td,%s\r",args[2][2]))
+        self:write("tc\r")
+        self:wait(50)
+        self:write("ts,1,1\r")
+        self:wait(50)
+        self:write(f("td,%s\r",args[2][2]))
+        self:wait(100)
     end
     local function get_answer(args)
     end
-    switch(inst[1])
-        .case('MOVEP',movep)
-        .case('OPEN',open)
-        .case('CLOSE',close)
-        .case('HARDHOME',hardhome)
-        .case('HOME',home)
-        .case('VEL',vel)
-        .case('PAUSE',pause)
-        .case('OUTSIG',outsig)
-        .case('SEND',send)
-        .exec(inst)
+    local function validate_sig(sig)
+        if sig < 1 or sig > 8 then return false end
+        return true 
+    end
+    local function input_pending()
+        self:write("ss\r")
+        self:wait(100)
+        local ret,msg = self:read()
+        local status = tonumber(msg)
+        local pending = (status&16) >>4
+        return pending
+    end
+    local function waitfor(args)
+        for i=1,#args[2] do
+            local sig = self.eval:eval(args[2][i])
+            Runtime.assert(validate_sig(math.abs(sig)),args[1],'Out of Bounds')
+            if sig > 0 then
+                self:write(f("wi,%s,%d\r",sig,1))
+            else
+                self:write(f("wi,%s,%d\r",math.abs(sig),0))
+            end
+        end
+        while(input_pending()==1)do end
+    end
+    local function outsig(args)
+        local out = 0
+        for i=1,#args[2] do
+            local sig = self.eval:eval(args[2][i])
+            Runtime.assert(validate_sig(math.abs(sig)),args[1],'Out of Bounds')
+            if sig > 0 then
+                out = out | 1 << (sig-1)
+            end
+        end
+        self:write(f("op,%s\r",out))
+    end
+    local function ifsig(args)
+        self:write("ip\r")
+        self:wait(100)
+        local ret,msg = self:read()
+        local state = tonumber(msg)
+        local result = 1
+        for i=1,#args[2] do
+            local sig = self.eval:eval(args[2][i])
+            Runtime.assert(validate_sig(math.abs(sig)),args[1],'Out of Bounds')
+            local level = 0
+            if sig > 0 then level = 1 else level = 0 end
+            sig = math.abs(sig)
+            result = result and ((state&2^(sig-1))>>(sig-1))
+        end
+        args[3] = result
+    end
+
+    return Switch:new():build()
+        :case("MOVE",move)
+        :case('MOVEP',movep)
+        :case('OPEN',open)
+        :case('CLOSE',close)
+        :case('VEL',vel)
+        :case('HARDHOME',hardhome)
+        :case('HOME',home)
+        :case('WAITFOR',waitfor)
+        :case('IFSIG',ifsig)
+        :case('OUTSIG',outsig)
+        :case('PAUSE',pause)
+        :case('SEND',send)
+        :default(function(args) 
+            Runtime.assert(false,'--',"Operation "..args[1].." not implemented") 
+        end)
+end
+
+function Emitter:exec(inst)
+    local operation = self:register()
+    return operation:switch(inst[1],inst)
 end
 
 function main()
@@ -266,7 +334,10 @@ function main()
     local parser = Parser:new(tokenizer:tokenize())
     local ops = parser:parse()
     parser:dump()
-    local interpreter = Interpreter:new(parser.ops)
+    local serial = SerialPort:new(arg[2])
+    local emitter = Emitter:new()
+    emitter:setport(serial)
+    local interpreter = Interpreter:new(parser.ops,emitter)
     interpreter:run()
 end
 
